8b91c7ff6cf45fc81738d18da8b3f0a47e277b3c
Rene Jablonski <rene.jablonski@jkaref.com>
2016-03-15 14:19:12 +0100

    * SF

--- src/main/java/com/rcs/portlet/slider/util/SliderParamUtil.java
+++ src/main/java/com/rcs/portlet/slider/util/SliderParamUtil.java
@@ -11,17 +11,17 @@ public interface SliderParamUtil {
     String SETTINGS_PAUSE_TIME           = "settings-pause-time";
     String SETTINGS_START_SLIDE          = "settings-start-slide";
     String SETTINGS_RANDOM_SLIDE         = "settings-random-slide";
-    String SETTINGS_NUMBER_ANIMATE       = "settings-number-animate";//<<
+    String SETTINGS_NUMBER_ANIMATE       = "settings-number-animate";
     
     /* look and feel */
     String SETTINGS_THEME                = "settings-theme";
     String SETTINGS_ADDITIONAL_CSS_CLASS = "settings-addition-css-class";
-    String SETTINGS_NUMBER_SLIDES        = "settings-number-slides";//<<
+    String SETTINGS_NUMBER_SLIDES        = "settings-number-slides";
     String SETTINGS_SLIDER_WIDTH         = "settings-slider-width";
     String SETTINGS_SLIDER_HEIGHT        = "settings-slider-height";
     String SETTINGS_SLIDER_ITEM_WIDTH    = "settings-slider-item-width";
     String SETTINGS_SLIDER_ITEM_HEIGHT   = "settings-slider-item-height";
-    String SETTINGS_DISABLE_CAPTION      = "settings-disable-catpion";//<<
+    String SETTINGS_DISABLE_CAPTION      = "settings-disable-caption";
     String SETTINGS_CAPTION_WIDTH        = "settings-caption-width";
     String SETTINGS_CAPTION_HEIGHT       = "settings-caption-height";
     String SETTINGS_CAPTION_ITEM_WIDTH   = "settings-caption-item-width";
